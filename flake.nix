dd27ccdaa1e81e3b73dc79dda8aecdb0b6091a66
Lova Andriarimalala <43842786+Xpirix@users.noreply.github.com>
2024-11-19 12:02:50 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -80,23 +80,23 @@
     # }
 
     packages = {
-      x86_64-linux = {
-        default = pkgsFor "x86_64-linux".callPackage ./packages/utils {};
-        setup-zfs-machine = pkgsFor "x86_64-linux".callPackage ./packages/setup-zfs-machine {};
-        qgis-custom = pkgsFor "x86_64-linux".qgis.overrideAttrs (oldAttrs: rec {
+      x86_64-linux = let pkgs = pkgsFor "x86_64-linux"; in {
+        default = pkgs.callPackage ./packages/utils {};
+        setup-zfs-machine = pkgs.callPackage ./packages/setup-zfs-machine {};
+        qgis-custom = pkgs.qgis.overrideAttrs (oldAttrs: rec {
           pythonBuildInputs =
             oldAttrs.pythonBuildInputs
-            ++ [pkgsFor "x86_64-linux".numpy pkgsFor "x86_64-linux".requests pkgsFor "x86_64-linux".future pkgsFor "x86_64-linux".matplotlib pkgsFor "x86_64-linux".pandas pkgsFor "x86_64-linux".geopandas pkgsFor "x86_64-linux".plotly pkgsFor "x86_64-linux".pyqt5_with_qtwebkit pkgsFor "x86_64-linux".pyqtgraph pkgsFor "x86_64-linux".rasterio pkgsFor "x86_64-linux".sqlalchemy];
+            ++ [pkgs.numpy pkgs.requests pkgs.future pkgs.matplotlib pkgs.pandas pkgs.geopandas pkgs.plotly pkgs.pyqt5_with_qtwebkit pkgs.pyqtgraph pkgs.rasterio pkgs.sqlalchemy];
         });
-        tilemaker = pkgsFor "x86_64-linux".callPackage ./packages/tilemaker {};
-        gverify = pkgsFor "x86_64-linux".callPackage ./packages/gverify {};
-        itk4 = pkgsFor "x86_64-linux".callPackage ./packages/itk4 {};
-        otb = pkgsFor "x86_64-linux".callPackage ./packages/otb { self = self; };
-        distrobox = pkgsFor "x86_64-linux".callPackage ./packages/distrobox {};
-        kartoza-plymouth = pkgsFor "x86_64-linux".callPackage ./packages/kartoza-plymouth {};
-        kartoza-grub = pkgsFor "x86_64-linux".callPackage ./packages/kartoza-grub {};
-        kartoza-cron = pkgsFor "x86_64-linux".callPackage ./packages/kartoza-cron {};
-        qgis-conda = pkgsFor "x86_64-linux".callPackage ./packages/qgis-conda {};
+        tilemaker = pkgs.callPackage ./packages/tilemaker {};
+        gverify = pkgs.callPackage ./packages/gverify {};
+        itk4 = pkgs.callPackage ./packages/itk4 {};
+        otb = pkgs.callPackage ./packages/otb { self = self; };
+        distrobox = pkgs.callPackage ./packages/distrobox {};
+        kartoza-plymouth = pkgs.callPackage ./packages/kartoza-plymouth {};
+        kartoza-grub = pkgs.callPackage ./packages/kartoza-grub {};
+        kartoza-cron = pkgs.callPackage ./packages/kartoza-cron {};
+        qgis-conda = pkgs.callPackage ./packages/qgis-conda {};
         iso = nixos-generators.nixosGenerate {
           inherit pkgs;
           modules = [./installer-configuration.nix ./software/system/kartoza-plymouth.nix ./software/system/kartoza-grub.nix ./software/system/ssh.nix];
@@ -109,23 +109,23 @@
         };
       };
 
-      aarch64-linux = {
-        default = pkgsFor "aarch64-linux".callPackage ./packages/utils {};
-        setup-zfs-machine = pkgsFor "aarch64-linux".callPackage ./packages/setup-zfs-machine {};
-        qgis-custom = pkgsFor "aarch64-linux".qgis.overrideAttrs (oldAttrs: rec {
+      aarch64-linux = let pkgs = pkgsFor "aarch64-linux"; in {
+        default = pkgs.callPackage ./packages/utils {};
+        setup-zfs-machine = pkgs.callPackage ./packages/setup-zfs-machine {};
+        qgis-custom = pkgs.qgis.overrideAttrs (oldAttrs: rec {
           pythonBuildInputs =
             oldAttrs.pythonBuildInputs
-            ++ [pkgsFor "aarch64-linux".numpy pkgsFor "aarch64-linux".requests pkgsFor "aarch64-linux".future pkgsFor "aarch64-linux".matplotlib pkgsFor "aarch64-linux".pandas pkgsFor "aarch64-linux".geopandas pkgsFor "aarch64-linux".plotly pkgsFor "aarch64-linux".pyqt5_with_qtwebkit pkgsFor "aarch64-linux".pyqtgraph pkgsFor "aarch64-linux".rasterio pkgsFor "aarch64-linux".sqlalchemy];
+            ++ [pkgs.numpy pkgs.requests pkgs.future pkgs.matplotlib pkgs.pandas pkgs.geopandas pkgs.plotly pkgs.pyqt5_with_qtwebkit pkgs.pyqtgraph pkgs.rasterio pkgs.sqlalchemy];
         });
-        tilemaker = pkgsFor "aarch64-linux".callPackage ./packages/tilemaker {};
-        gverify = pkgsFor "aarch64-linux".callPackage ./packages/gverify {};
-        itk4 = pkgsFor "aarch64-linux".callPackage ./packages/itk4 {};
-        otb = pkgsFor "aarch64-linux".callPackage ./packages/otb { self = self; };
-        distrobox = pkgsFor "aarch64-linux".callPackage ./packages/distrobox {};
-        kartoza-plymouth = pkgsFor "aarch64-linux".callPackage ./packages/kartoza-plymouth {};
-        kartoza-grub = pkgsFor "aarch64-linux".callPackage ./packages/kartoza-grub {};
-        kartoza-cron = pkgsFor "aarch64-linux".callPackage ./packages/kartoza-cron {};
-        qgis-conda = pkgsFor "aarch64-linux".callPackage ./packages/qgis-conda {};
+        tilemaker = pkgs.callPackage ./packages/tilemaker {};
+        gverify = pkgs.callPackage ./packages/gverify {};
+        itk4 = pkgs.callPackage ./packages/itk4 {};
+        otb = pkgs.callPackage ./packages/otb { self = self; };
+        distrobox = pkgs.callPackage ./packages/distrobox {};
+        kartoza-plymouth = pkgs.callPackage ./packages/kartoza-plymouth {};
+        kartoza-grub = pkgs.callPackage ./packages/kartoza-grub {};
+        kartoza-cron = pkgs.callPackage ./packages/kartoza-cron {};
+        qgis-conda = pkgs.callPackage ./packages/qgis-conda {};
         iso = nixos-generators.nixosGenerate {
           inherit pkgs;
           modules = [./installer-configuration.nix ./software/system/kartoza-plymouth.nix ./software/system/kartoza-grub.nix ./software/system/ssh.nix];
@@ -145,39 +145,42 @@
     ##
     ######################################################
 
-    nixosConfigurations = supportedSystems // (system: {
-      # Live iso Generation
-      # Please read: https://nixos.wiki/wiki/Creating_a_NixOS_live_CD
-      # To build:
-      # nix build .#nixosConfigurations.live.config.system.build.isoImage
-      # To run:
-      # qemu-system-${system} -enable-kvm -m 8096 -cdrom result/iso/nixos-*.iso
-      live = pkgsFor system.lib.nixosSystem {
-        specialArgs = specialArgsFor system;
-        inherit system;
-        modules =
-          [
-            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-            isoBaseFor system
-          ]
-          ++ sharedModulesFor system
-          ++ [./hosts/iso-gnome.nix];
-      };
+    nixosConfigurations = builtins.listToAttrs (map (system: {
+      name = system;
+      value = {
+        # Live iso Generation
+        # Please read: https://nixos.wiki/wiki/Creating_a_NixOS_live_CD
+        # To build:
+        # nix build .#nixosConfigurations.${system}.config.system.build.isoImage
+        # To run:
+        # qemu-system-${system} -enable-kvm -m 8096 -cdrom result/iso/nixos-*.iso
+        live = pkgsFor system.lib.nixosSystem {
+          specialArgs = specialArgsFor system;
+          inherit system;
+          modules =
+            [
+              "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+              isoBaseFor system
+            ]
+            ++ sharedModulesFor system
+            ++ [./hosts/iso-gnome.nix];
+        };
 
-      crest = makeHostFor system "crest"; # Tim's p14s thinkpad - love this machine!
-      waterfall = makeHostFor system "waterfall"; # Tim Tuxedo desktop box
-      valley = makeHostFor system "valley"; # Tim headless box
-      delta = makeHostFor system "delta"; # Amy Laptop
-      lagoon = makeHostFor system "lagoon"; # Vicky laptop
-      plain = makeHostFor system "plain"; # Marina laptop
-      rock = makeHostFor system "rock"; # Virtman manual testbed
-      jeff = makeHostFor system "jeff"; # Jeff - running plasma
-      atoll = makeHostFor system "atoll"; # Dorah's Laptop
-      crater = makeHostFor system "crater"; # Eli's Laptop
-      test-gnome-full = makeHostFor system "test-gnome-full"; # Automated testbed - test gnome
-      test-gnome-minimal = makeHostFor system "test-gnome-minimal"; # Automated testbed - test gnome
-      test-kde6 = makeHostFor system "test-kde6"; # Automated testbed - test kde6
-      test-kde5 = makeHostFor system "test-kde5"; # Automated testbed - test kde5
-    });
+        crest = makeHostFor system "crest"; # Tim's p14s thinkpad - love this machine!
+        waterfall = makeHostFor system "waterfall"; # Tim Tuxedo desktop box
+        valley = makeHostFor system "valley"; # Tim headless box
+        delta = makeHostFor system "delta"; # Amy Laptop
+        lagoon = makeHostFor system "lagoon"; # Vicky laptop
+        plain = makeHostFor system "plain"; # Marina laptop
+        rock = makeHostFor system "rock"; # Virtman manual testbed
+        jeff = makeHostFor system "jeff"; # Jeff - running plasma
+        atoll = makeHostFor system "atoll"; # Dorah's Laptop
+        crater = makeHostFor system "crater"; # Eli's Laptop
+        test-gnome-full = makeHostFor system "test-gnome-full"; # Automated testbed - test gnome
+        test-gnome-minimal = makeHostFor system "test-gnome-minimal"; # Automated testbed - test gnome
+        test-kde6 = makeHostFor system "test-kde6"; # Automated testbed - test kde6
+        test-kde5 = makeHostFor system "test-kde5"; # Automated testbed - test kde5
+      };
+    }) supportedSystems);
   };
 }
